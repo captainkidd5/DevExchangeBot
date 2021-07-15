a4e3445ff2c04dfe1c97555bca3d7769fd183054
Aeris <aeris.github@gmail.com>
2021-07-15 16:33:09 +0200

    Added starboard-related setting

--- src/Storage/StorageModel.cs
+++ src/Storage/StorageModel.cs
@@ -8,6 +8,10 @@ namespace DevExchangeBot.Storage
         public Dictionary<ulong, UserModel> Users { get; set; }
         public float ExpMultiplier { get; set; }
 
+        public ulong HeartBoardChannel { get; set; }
+        public bool HeartBoardEnabled { get; set; }
+        public IDictionary<ulong, ulong> HeartboardMessages { get; set; }
+
         public void AddUser(UserModel user)
         {
             Users.Add(user.Id, user);
