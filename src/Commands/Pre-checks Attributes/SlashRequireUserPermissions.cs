a9f815d401b75f21b4e602e8881edbd9784c22fa
Aeris <aeris.github@gmail.com>
2021-07-20 11:30:33 +0200

    Added a check base attribute for permission check

---
+++ src/Commands/Pre-checks Attributes/SlashRequireUserPermissions.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.SlashCommands;
+#pragma warning disable 1998
+
+namespace DevExchangeBot.Commands
+{
+    public class SlashRequireUserPermissions : SlashCheckBaseAttribute
+    {
+        // ReSharper disable once MemberCanBePrivate.Global
+        public readonly Permissions Perm;
+
+        public SlashRequireUserPermissions(Permissions perm)
+            => Perm = perm;
+
+        public override async Task<bool> ExecuteChecksAsync(InteractionContext ctx)
+            => ctx.Member.Permissions.HasPermission(Perm);
+    }
+}
