ebfea4098b1c6e02a796f010bdcc07729307a52f
Aeris <aeris.github@gmail.com>
2021-07-27 22:10:09 +0200

    Added choice providers for role menu commands

---
+++ src/Commands/Slash Commands Utilities/ChoicesProviders.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevExchangeBot.Storage;
+using DSharpPlus.Entities;
+#pragma warning disable 1998
+
+namespace DevExchangeBot.Commands
+{
+    public class MenuNamesChoiceProvider
+    {
+        // ReSharper disable once UnusedMember.Global
+        public async Task<IEnumerable<DiscordApplicationCommandOptionChoice>> Provider()
+        {
+            return StorageContext.Model.RoleMenus.Select(menu => new DiscordApplicationCommandOptionChoice(menu.Name, menu.Name)).ToList();
+        }
+    }
+
+    public class SelectionTypeChoiceProvider
+    {
+        // ReSharper disable once UnusedMember.Global
+        public async Task<IEnumerable<DiscordApplicationCommandOptionChoice>> Provider()
+        {
+            return new DiscordApplicationCommandOptionChoice[]
+            {
+                new("Single", "False"),
+                new("Multiple", "True")
+            };
+        }
+    }
+}
